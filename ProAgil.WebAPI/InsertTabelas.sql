f2529bd25351b9b265cceef61b23fdd78a07a2d0
Paulo <scluispaulo@gmail.br>
2020-11-21 19:31:57 -0300

    Populando banco

---
+++ ProAgil.WebAPI/InsertTabelas.sql
@@ -0,0 +1,114 @@
+INSERT INTO Eventos
+(
+	Local,
+	DataEvento,
+	Tema,
+	QtdPessoas,
+	ImagemURL,
+	Telefone,
+	Email
+) VALUES
+(
+	'Fortaleza',
+	'12/18/2020',
+	'Angular',
+	80,
+	'img1.jpg',
+	'8888-8888',
+	'angular@outlook.com'
+);
+
+INSERT INTO Eventos
+(
+	Local,
+	DataEvento,
+	Tema,
+	QtdPessoas,
+	ImagemURL,
+	Telefone,
+	Email
+) VALUES
+(
+	'Natal',
+	'12/15/2020',
+	'.Net',
+	50,
+	'img2.jpg',
+	'7777-8888',
+	'dotnet@gmail.com'
+);
+
+INSERT INTO Eventos
+(
+	Local,
+	DataEvento,
+	Tema,
+	QtdPessoas,
+	ImagemURL,
+	Telefone,
+	Email
+) VALUES
+(
+	'Recife',
+	'12/20/2020',
+	'.Net e Angular',
+	100,
+	'img3.jpg',
+	'8888-9999',
+	'evento@outlook.com'
+);
+
+
+INSERT INTO Lotes 
+(
+    Nome,
+    Preco,
+    DataInicio,
+    DataFim,
+    quantidade,
+    EventoId
+) VALUES
+(
+    'Promo I',
+    '19.9',
+    11/20/2020,
+    11/22/2020,
+    20,
+    1
+);
+
+INSERT INTO Lotes 
+(
+    Nome,
+    Preco,
+    DataInicio,
+    DataFim,
+    quantidade,
+    EventoId
+) VALUES
+(
+    'Promo II',
+    '39.9',
+    12/03/2020,
+    12/05/2020,
+    20,
+    2
+);
+
+INSERT INTO Lotes 
+(
+    Nome,
+    Preco,
+    DataInicio,
+    DataFim,
+    quantidade,
+    EventoId
+) VALUES
+(
+    'Promo I',
+    '45.9',
+    11/20/2020,
+    11/22/2020,
+    20,
+    3
+);
